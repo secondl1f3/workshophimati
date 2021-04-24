ff920836dbbecbf21b016d5fcab1e2aebe55a027
secondl1f3 <ahmad.fajar@outlook.co.id>
2021-04-24 17:35:14 +0700

    create controller

--- himati/src/main/java/com/workshop/himati/dto/Greeting.java
+++ himati/src/main/java/com/workshop/himati/dto/Greeting.java
@@ -1,4 +1,15 @@
 package com.workshop.himati.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Greeting {
+    private Long id;
+    private String name;
 }
